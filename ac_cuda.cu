422004f7ceae5e5249389f082435527db3fdd46e
Αργύρης Παπαδόπουλος <99400979+argyrisp@users.noreply.github.com>
2022-08-08 09:05:33 +0300

    Add files via upload

--- ac_cuda.cu
+++ ac_cuda.cu
@@ -1,7 +1,33 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
+
 #include <cuda.h>
+#include <assert.h>
+// #define ACCUDA_MAIN // uncomment this to run individually: nvcc -o ac_cuda ../src/sfutil/ac_cuda.cu
+#ifndef ACCUDA_MAIN
+#include "/home/zxcv/snort_2.9.20/snort-2.9.20/src/snort.h" // for PacketCount struct
+#endif
 
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 
+#include "ac_cuda.cu.h"
+
+#define MEM_OPTS
+#ifdef MEM_OPTS
+ #ifdef TEX_STATE_TABLE
+ static texture <acstate_t, 1, cudaReadModeElementType> texStateTable;
+ #endif
+#else
+static texture <acstate_t, 2, cudaReadModeElementType> texStateTable;
+#endif
 
 /*
 *  Text Data Buffer
@@ -9,15 +35,58 @@
 static unsigned char text[512];
 static const char *text_ptr = (const char*)text;
 
+extern unsigned int UsePinnedMem;
+#ifdef MEM_OPTS
+  typedef struct packet_buffer_ {
+    unsigned char bufNo; // 0 or 1
+    unsigned char buf[2][BUFFER_SIZE][PKT_SNAPLEN];
+    unsigned int bufPos;
+  } packet_buffer_t;
+
+  static Packet pktHdr[2][BUFFER_SIZE];
+
+  static PORT_GROUP *pktPg[2][BUFFER_SIZE];
+
+  static packet_buffer_t *packetBuffer = 0x00;
+
+  static acstate_t *stateTable = NULL;
+  static unsigned int stateTablePos = 0;
+
+
+  static cudaStream_t stream;
+  static unsigned int isStreamInitialized = 0;
+  #ifdef TEX_STATE_TABLE
+    static unsigned int stateTableSize = 131072; // 128MB; max number of states that can be bound to texture
+  #else
+    static unsigned int stateTableSize = 1048576; // 1GB;
+  #endif
+
+#endif
+
+
+static void print_packet_buffer_info() {
+  static int first_time=1;
+
+  if (first_time) {
+    first_time = 0;
+
+    fprintf(stdout, "Packet buffer [%s]: %d packets, %d bytes each (Total: %d bytes)\n",
+                        (UsePinnedMem) ? "PINNED" : "PAGEABLE",
+                        BUFFER_SIZE, PKT_SNAPLEN, BUFFER_SIZE * PKT_SNAPLEN);
+    fflush(stdout);
+  }
+}
+
 ACCUDA_STRUCT * acNew () {
   // print_packet_buffer_info();
 
   // if (isStreamInitialized == 0) {
 	//   cudaStreamCreate(&stream);
   //   #ifdef DEBUG
-  //   CUT_CHECK_ERROR("cudaStreamCreate");
+  //     CUT_CHECK_ERROR("cudaStreamCreate");
   //   #endif
   //   isStreamInitialized = 1;
+  // }
 }
 
 void printCommandInfo(int n, const char ** args){
@@ -25,11 +94,12 @@ void printCommandInfo(int n, const char ** args){
       printf("ARGUEMENT %d: %s\n", i, args[i]);
     }
 }
-#define ACCUDA_MAIN // uncomment this to run individually: nvcc -o ac_cuda ../src/sfutil/ac_cuda.cu
+//#define ACCUDA_MAIN 
 #ifdef ACCUDA_MAIN
 
 int main (int argc, const char **argv){
   int i, nocase = 0;
+  ACCUDA_STRUCT * ac;
 
   if (argc < 4)
   {
@@ -45,6 +115,8 @@ int main (int argc, const char **argv){
     if (strcmp (argv[i], "-nocase") == 0)
       nocase = 1;
   printf("Text string: %s\n", text);
+
+  ac = acNew();
 
 
   for (i = 3; i < argc; i++)
--- ac_cuda.cu.h
+++ ac_cuda.cu.h
@@ -2,4 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 #include <cuda.h>
+#include "acsmx.h"
 
+
+#define MEM_OPTS
+#define ALPHABET_SIZE 256
+#define BUFFER_SIZE (8192)
+#define PKT_SNAPLEN	1540
+
+typedef short acstate_t;
+
+typedef struct _ac_pattern {      
+    //struct _ac_pattern *next;
+    //unsigned char *patrn;
+    unsigned char *casepatrn;
+    //unsigned long *pattern_mask;
+    int n;
+    int nocase;
+    int offset;
+    int depth;
+    void* id;
+    int iid;
+} ACCUDA_PATTERN_ATTR;
+
+typedef struct {
+    ACSM_STRUCT *acsm;
+
+    cudaArray *acPatternsArray_d;
+    #ifdef MEM_OPTS
+        acstate_t *acStateTableArray_d;
+        unsigned int acStateTableOffset;
+    #else
+    	cudaArray *acStateTableArray_d;
+    #endif
+
+    unsigned int		acPatternSize;
+	unsigned int		acPatternCurr;
+
+	unsigned int		maxPatternLen; // the maximum pattern length
+    #ifndef MEM_OPTS
+        #ifdef DOUBLE_BUFFER
+            unsigned char		bufNo; // 0 or 1
+            unsigned char		buf[2][BUFFER_SIZE][PKT_SNAPLEN];
+        #else
+            unsigned char		bufNo; // 0
+            unsigned char		buf[1][BUFFER_SIZE][PKT_SNAPLEN]; // buffer to copy packets + size
+        #endif
+    #endif
+    unsigned int *retval;
+	unsigned int *retval_d;
+    #ifndef MEM_OPTS
+	    unsigned int bufCurr;
+    #endif
+    unsigned acPatternCnt;		/* obsolete by kmpPatternCurr */
+} ACCUDA_STRUCT;
+
+
+ACCUDA_STRUCT * acNew (void);
